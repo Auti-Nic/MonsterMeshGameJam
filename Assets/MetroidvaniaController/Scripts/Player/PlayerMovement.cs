5678e9cf5807f93354e87d5c0482948230a4a540
Kevin Nguyen <kevin.nguyen2511@gmail.com>
2021-12-17 10:42:21 +1100

    Hiding Cabinet

--- Assets/MetroidvaniaController/Scripts/Player/PlayerMovement.cs
+++ Assets/MetroidvaniaController/Scripts/Player/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour {
 
 	public CharacterController2D controller;
 	public Animator animator;
+	public Rigidbody2D rb;
+	public SpriteRenderer sr;
 
 	public float runSpeed = 40f;
 
@@ -13,14 +15,39 @@ public class PlayerMovement : MonoBehaviour {
 	bool jump = false;
 	bool dash = false;
 
+	public bool isHidden = false;
+
 	//bool dashAxis = false;
 	
 	// Update is called once per frame
 	void Update () {
 
+		if (isHidden)
+        {
+			rb.constraints = RigidbodyConstraints2D.FreezeAll;
+			sr.enabled = false;
+		}
+        else
+        {
+			sr.enabled = true;
+			rb.constraints = RigidbodyConstraints2D.None;
+			rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+		}
+
+
 		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
 		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+
+		/*
+        if (isHidden)
+        {
+			if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown("w"))
+            {
+				isHidden = false;
+            }
+		}*/
 
 		/*if (Input.GetKeyDown(KeyCode.Z))
 		{
