be1166f691457a20a6376ab3d77afc72c6ddad7c
Alexander Minton <alex.minton@hotmail.com>
2021-12-16 19:11:41 +1100

    Added 2 minute timer to levels

--- Assets/Scripts/Framework/GameController.cs
+++ Assets/Scripts/Framework/GameController.cs
@@ -8,7 +8,10 @@ public class GameController : MonoBehaviour
     public static GameController instance;
     public int organsAcquired;
     enum GameState { MainMenu, Level, Cutscene, End};
+    public float timeLeftInMission;
+    float timeForLevel = 120; //Time to complete level
     GameState currentState = GameState.MainMenu;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +22,11 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentState == GameState.Level)
+        {
+            timeLeftInMission -= Time.deltaTime;
+            if (timeLeftInMission <= 0) TimeRanOut();
+        }
     }
 
     public void ProgressState()
@@ -39,6 +46,7 @@ public class GameController : MonoBehaviour
     public void CompleteLevel()
     {
         organsAcquired++;
+        currentState = GameState.Cutscene;
         LoadLevel("Cutscenes");
         //BeginCutscene(organsAcquired);
         //CutsceneController.instance.BeginCutscene(organsAcquired);
@@ -50,6 +58,13 @@ public class GameController : MonoBehaviour
     }
     public void BeginNextLevel()
     {
+        timeLeftInMission = timeForLevel; //2 minutes
+        currentState = GameState.Level;
         LoadLevel(organsAcquired);
-    }    
+    }
+    public void TimeRanOut()
+    {
+        currentState = GameState.End;
+        GameController.instance.LoadLevel("GameOver");
+    }
 }
