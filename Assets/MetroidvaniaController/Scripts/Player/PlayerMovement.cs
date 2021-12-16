3a3ab04bc65a9efb67c89efdf8063594386ec70f
Alexander Minton <alex.minton@hotmail.com>
2021-12-16 18:50:48 +1100

    Initial player movement with detection. Bug (graphical?) on detection not going above 50%

--- Assets/MetroidvaniaController/Scripts/Player/PlayerMovement.cs
+++ Assets/MetroidvaniaController/Scripts/Player/PlayerMovement.cs
@@ -22,7 +22,7 @@ public class PlayerMovement : MonoBehaviour {
 
 		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
-		if (Input.GetKeyDown(KeyCode.Z))
+		/*if (Input.GetKeyDown(KeyCode.Z))
 		{
 			jump = true;
 		}
@@ -30,7 +30,7 @@ public class PlayerMovement : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.C))
 		{
 			dash = true;
-		}
+		}*/
 
 		/*if (Input.GetAxisRaw("Dash") == 1 || Input.GetAxisRaw("Dash") == -1) //RT in Unity 2017 = -1, RT in Unity 2019 = 1
 		{
